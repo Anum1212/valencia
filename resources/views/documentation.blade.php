508f37a502d5aa4950f57fd50e1c2f46289f11b6
Anum1212 <anamamer0@gmail.com>
2018-02-09 11:43:10 +0500

    changed video size css for better viewing on mobile

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -55,21 +55,24 @@
     <div id="functionality" class="section">
       <div id="siteOverView">
         <!-- Home Page -->
+        <div class="item">
         <b class="itemHeading"><h4>Home Page</h4></b>
         <div class="itemVideo">
-          <video width="650" controls>
+          <video controls>
     <source src="/storage/myAssets/demo/homePage.mp4"  type="video/mp4">
     Your browser does not support HTML5 video.
   </video>
         </div>
-        <br>
+        </div>
         <!-- Admin Page -->
+        <div class="item">
         <b class="itemHeading"><h4>Admin Page</h4></b>
         <div class="itemVideo">
-          <video width="650" controls>
+          <video controls>
     <source src="/storage/myAssets/demo/adminPage.mp4"  type="video/mp4">
     Your browser does not support HTML5 video.
   </video>
+        </div>
         </div>
       </div>
       <br>
@@ -84,7 +87,7 @@
       <b class="itemHeading"><h4>News</h4></b>
       <p>Add news to home page to let people know about upcoming events. Can attatch image and/or file</p>
       <div class="itemVideo">
-        <video width="650" controls>
+        <video controls>
   <source src="/storage/myAssets/demo/addAnnouncement.mp4"  type="video/mp4">
   Your browser does not support HTML5 video.
 </video>
@@ -96,7 +99,7 @@
       <b class="itemHeading"><h4>Images</h4></b>
       <p>Add images to different sliders to let people know more about the society</p>
       <div class="itemVideo">
-        <video width="650" controls>
+        <video controls>
   <source src="/storage/myAssets/demo/addImages.mp4"  type="video/mp4">
   Your browser does not support HTML5 video.
 </video>
@@ -108,7 +111,7 @@
       <b class="itemHeading"><h4>Circulars</h4></b>
       <p>Upload circulars so people can download important society related circulars</p>
       <div class="itemVideo">
-        <video width="650" controls>
+        <video controls>
   <source src="/storage/myAssets/demo/addCircular.mp4"  type="video/mp4">
   Your browser does not support HTML5 video.
 </video>
@@ -120,7 +123,7 @@
       <b class="itemHeading"><h4>Members</h4></b>
       <p>Add details about members to let people know more about the people responsible for managing the housing society.</p>
       <div class="itemVideo">
-        <video width="650" controls>
+        <video controls>
   <source src="/storage/myAssets/demo/addMember.mp4"  type="video/mp4">
   Your browser does not support HTML5 video.
 </video>
@@ -132,7 +135,7 @@
       <b class="itemHeading"><h4>Messages</h4></b>
       <p>Site visitors can contact site admin for query or complaint and the site admin can respond to them using built-in message system. The visitor would receive the reply through email</p>
       <div class="itemVideo">
-        <video width="650" controls>
+        <video controls>
   <source src="/storage/myAssets/demo/addMessage.mp4"  type="video/mp4">
   Your browser does not support HTML5 video.
 </video>
@@ -150,7 +153,7 @@
       <b class="itemHeading"><h4>News i.e change title or change uploaded file</h4></b>
       <p>News title, content, image, file can be edited</p>
       <div class="itemVideo">
-        <video width="650" controls>
+        <video controls>
   <source src="/storage/myAssets/demo/editAnnouncement.mp4"  type="video/mp4">
   Your browser does not support HTML5 video.
 </video>
@@ -161,10 +164,12 @@
       <div class="item">
       <b class="itemHeading"><h4>Images</h4></b>
       <p>image titles can be changed and images can be moved to different image sliders(achievements, onGoing projects, gallery or main home slider)</p>
-      <div class="itemVideo">  <video width="650" controls>
+      <div class="itemVideo">
+        <video controls>
   <source src="/storage/myAssets/demo/editImages.mp4"  type="video/mp4">
   Your browser does not support HTML5 video.
-</video></div>
+</video>
+</div>
       </div>
 
       <!-- Circulars -->
@@ -177,30 +182,36 @@
       <div class="item">
       <b class="itemHeading"><h4>Members</h4></b>
       <p>Members details can be edited, they can be moved to different member types (Committe, Focal)</p>
-      <div class="itemVideo">  <video width="650" controls>
+      <div class="itemVideo">
+        <video controls>
   <source src="/storage/myAssets/demo/editMember.mp4"  type="video/mp4">
   Your browser does not support HTML5 video.
-</video></div>
+</video>
+</div>
     </div>
 
     <!-- Members -->
     <div class="item">
     <b class="itemHeading"><h4>Members</h4></b>
     <p>Members details can be edited, they can be moved to different member types (Committe, Focal)</p>
-    <div class="itemVideo">  <video width="650" controls>
+    <div class="itemVideo">
+      <video controls>
 <source src="/storage/myAssets/demo/editMember.mp4"  type="video/mp4">
 Your browser does not support HTML5 video.
-</video></div>
+</vide
+o></div>
   </div>
 
     <!-- Admin Profile -->
     <div class="item">
     <b class="itemHeading"><h4>Admin Profile</h4></b>
     <p>Admin can edit their name, email or password</p>
-    <div class="itemVideo">  <video width="650" controls>
+    <div class="itemVideo">
+      <video controls>
 <source src="/storage/myAssets/demo/adminProfile.mp4"  type="video/mp4">
 Your browser does not support HTML5 video.
-</video></div>
+</vide
+o></div>
   </div>
 
   </div>
@@ -215,16 +226,20 @@ Your browser does not support HTML5 video.
               {{-- delete --}}
               <p>Delete</p>
               <div class="itemVideo">
-                <div class="itemVideo">  <video width="650" controls>
+                <div class="itemVideo">
+                  <video controls>
             <source src="/storage/myAssets/demo/delete.mp4"  type="video/mp4">
             Your browser does not support HTML5 video.
-          </video></div>
+          </video>
+        </div>
           {{-- disable --}}
           <p>Disable</p>
-          <div class="itemVideo">  <video width="650" controls>
+          <div class="itemVideo">
+            <video controls>
       <source src="/storage/myAssets/demo/disable.mp4"  type="video/mp4">
       Your browser does not support HTML5 video.
-    </video></div>
+    </video>
+  </div>
               </div>
             </div>
     </div>
